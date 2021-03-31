ef4b18c157316b6be64c3dc6d2960232f7d36a17
Numan <numan.ali1@gmail.com>
2021-03-31 16:11:03 +0500

    initial

---
+++ src/app/kaur9094.spec.ts
@@ -0,0 +1,7 @@
+import { Kaur9094 } from './kaur9094';
+
+describe('Kaur9094', () => {
+  it('should create an instance', () => {
+    expect(new Kaur9094()).toBeTruthy();
+  });
+});
---
+++ src/app/kaur9094.ts
@@ -0,0 +1,2 @@
+export class Kaur9094 {
+}
