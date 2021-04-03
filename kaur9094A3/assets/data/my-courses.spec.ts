50d5614063a98ae0a298b3ac8179c5db4ba84d08
Numan <numan.ali1@gmail.com>
2021-04-03 22:51:17 +0500

    Auto-generated commit

---
+++ kaur9094A3/assets/data/my-courses.spec.ts
@@ -0,0 +1,7 @@
+import { MyCourses } from './my-courses';
+
+describe('MyCourses', () => {
+  it('should create an instance', () => {
+    expect(new MyCourses()).toBeTruthy();
+  });
+});
---
+++ kaur9094A3/assets/data/my-courses.ts
@@ -0,0 +1,31 @@
+import { CourseInfo } from '../../app/course-info';
+
+export const MYCOURSE: CourseInfo[] =
+[
+
+{course_code: 'SYST24444',
+course_title: 'Mobile Web-Based Applications', 
+day_of_week: 'Friday',
+professor_name: 'Andy Pak'},
+
+{course_code: 'LITT10918G',
+course_title: 'World Mythology ',
+day_of_week: 'Tuesday ',
+professor_name: 'Chrisoula Benak'},
+
+{course_code: 'DBAS32100', 
+course_title: 'Database Management',
+day_of_week: 'Monday ',
+professor_name: 'Walid Belal'},
+
+{course_code: 'PROG32356',
+course_title: '.NET Technologies using C#',
+day_of_week: 'Tuesday and Thursday',
+professor_name: 'Huy Pham'}
+
+
+];
+
+
+export class MyCourses {
+}
